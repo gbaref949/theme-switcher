366a810dbebfedbfe63fda8b891638545cb22cae
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-02-01 20:35:31 -0700

    base files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Footer from '/components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+      <main className="App-main">
+        <Main />
+      </main>
+      <footer className="App-footer">
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
